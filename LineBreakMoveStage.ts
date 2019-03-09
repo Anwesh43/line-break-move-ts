1cfc80bd21f7a10c82777dfd51203a9fa4137e3c
Anwesh43 <anweshthecool0@gmail.com>
2019-03-10 00:24:34 +0530

    adding D drawLBMode canvas method

--- LineBreakMoveStage.ts
+++ LineBreakMoveStage.ts
@@ -14,7 +14,33 @@ const maxScale : Function = (scale : number, i : number, n : number) : number =>
 const divideScale : Function = (scale : number, i : number, n : number) : number => Math.min(1 / n, maxScale(scale, i, n)) * n
 const mirrorValue : Function = (scale : number, a : number, b : number) : number => (1 - scaleFactor(scale)) / a + scaleFactor(scale) / b
 const updateValue : Function = (scale : number, dir : number, a : number, b : number) => {
-    return mirrorValue(scale, a, b) * dir * scGap 
+    return mirrorValue(scale, a, b) * dir * scGap
+}
+
+const drawLBMNode : Function = (context : CanvasRenderingContext2D, i : number, scale : number) => {
+    const gap : number = h / (nodes + 1)
+    const sc1 : number = divideScale(scale, 0, 2)
+    const sc2 : number = divideScale(scale, 1, 2)
+    const size : number = gap / sizeFactor
+    context.strokeStyle = foreColor
+    context.lineCap = 'round'
+    context.lineWidth = Math.min(w, h) / strokeFactor
+    context.save()
+    context.translate(w / 2, gap * (i + 1))
+    for (var j = 0; j < lines; j++) {
+        const sf : number = 1 - 2 * j
+        const sc1j : number = divideScale(scale, 0, 2)
+        const sc2j : number = divideScale(scale, 1, 2)
+        context.save()
+        context.translate(w / 2 * sf * sc2j, 0)
+        context.rotate(Math.PI/2 * sc1j)
+        context.beginPath()
+        context.moveTo(0, 0)
+        context.lineTo(size * sf, 0)
+        context.stroke()
+        context.restore()
+    }
+    context.restore()
 }
 
 class LineBreakMoveStage {
