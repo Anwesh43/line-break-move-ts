2e64d0534ab90b4c35bcbc31ac7ad4ab07d17ea1
Anwesh43 <anweshthecool0@gmail.com>
2019-03-10 00:38:37 +0530

    adding LBMNode class

--- LineBreakMoveStage.ts
+++ LineBreakMoveStage.ts
@@ -114,3 +114,47 @@ class Animator {
         }
     }
 }
+
+class LBMNode {
+    state : State = new State()
+    prev : LBMNode
+    next : LBMNode
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new LBMNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawLBMNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LBMNode {
+        var curr : LBMNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this 
+    }
+}
