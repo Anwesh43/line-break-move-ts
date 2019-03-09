a4cca5ab4557fa53c3a8b2911249e0cc1a6f0b99
Anwesh43 <anweshthecool0@gmail.com>
2019-03-09 22:20:50 +0530

    adding scale methods

--- LineBreakMoveStage.ts
+++ LineBreakMoveStage.ts
@@ -9,6 +9,14 @@ const sizeFactor : number = 2.9
 const foreColor : string = "#673AB7"
 const backColor : string = "#BDBDBD"
 
+const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
+const maxScale : Function = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
+const divideScale : Function = (scale : number, i : number, n : number) : number => Math.min(1 / n, maxScale(scale, i, n)) * n
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => (1 - scaleFactor(scale)) / a + scaleFactor(scale) / b
+const updateValue : Function = (scale : number, dir : number, a : number, b : number) => {
+    return mirrorValue(scale, a, b) * dir * scGap 
+}
+
 class LineBreakMoveStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
